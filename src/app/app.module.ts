d1fa66414159aa5b59f7c96214814de3fe66cb2a
sg <sreveman-work@yahoo.fr>
2019-02-23 00:43:02 +0100

    First router implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component'
+import { Routes, RouterModule } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonPremierComponent,
     AppareilComponent,
     AuthComponent,
     AppareilViewComponent
@@ -21,7 +27,8 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AppareilService],
   bootstrap: [AppComponent]
